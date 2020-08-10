12beea15abfd44c5afb9ce0f40e2ef3cd705d807
YNishidate <nishidate-yoshio2020@g.ecc.u-tokyo.ac.jp>
2020-08-10 12:02:01 +0000

    説明

--- 1/test11.cc
+++ 1/test11.cc
@@ -2,6 +2,6 @@
 using namespace std;
 int main(){
     const int i=5;
-    static_assert(8<=i, "i is too small\n");
+    static_assert(8<=i, "i is too small\n");    //asserts_error
     return 0;
 }
